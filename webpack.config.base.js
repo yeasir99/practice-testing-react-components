9658ec349cfda59c36c8d99abed9c9e105193f88
Yeasir Ahamed <yahamed007@gmail.com>
2021-11-27 01:28:03 +0600

    add webpack config

---
+++ webpack.config.base.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.html',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
