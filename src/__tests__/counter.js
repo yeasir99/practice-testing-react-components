4cbce7294d788e7486653dc1b9ee06c66ba5da3e
Yeasir Ahamed <yahamed007@gmail.com>
2021-12-01 07:26:19 +0600

    update counter test

--- src/__tests__/counter.js
+++ src/__tests__/counter.js
@@ -1,6 +1,15 @@
-import { render } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Counter from '../components/Counter'
 
 test('Counter increments and decrements when the buttons are clicked', () => {
   render(<Counter />)
+
+  const message = screen.getByText(/current count/i)
+  expect(message).toHaveTextContent('0')
+
+  fireEvent.click(screen.getByRole('button', { name: /increment/i }))
+  expect(message).toHaveTextContent('1')
+
+  fireEvent.click(screen.getByRole('button', { name: /decrement/i }))
+  expect(message).toHaveTextContent('0')
 })
