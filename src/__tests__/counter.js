cb16e1c83a4677f40024d8d348f94142bfd43efd
Yeasir Ahamed <yahamed007@gmail.com>
2021-12-06 13:42:52 +0600

    add another test in counter

--- src/__tests__/counter.js
+++ src/__tests__/counter.js
@@ -1,5 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { render, screen, fireEvent } from '@testing-library/react'
 import Counter from '../components/Counter'
+
+// without using @testing-library/react
+// old version
+
+test('test Counter', () => {
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(<Counter />, div)
+
+  const [increment, decrement] = div.querySelectorAll('button')
+  const message = div.firstChild.querySelector('div')
+
+  expect(message.textContent).toBe('Current count 0')
+
+  increment.click()
+  expect(message.textContent).toBe('Current count 1')
+
+  decrement.click()
+  expect(message.textContent).toBe('Current count 0')
+
+  div.remove()
+})
+
+// Using @testing-library/react
+// new version
 
 test('Counter increments and decrements when the buttons are clicked', () => {
   render(<Counter />)
